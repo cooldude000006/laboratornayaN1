44b643993b7713077c18e87bcdb0662aeff49267
cooldude000006 <kivan000009@gmail.com>
2026-03-20 22:16:57 +0300

    Update int_type.c

--- int_type.c
+++ int_type.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
+//выделение памяти
 void* alloc_int(void) {
     int* ptr = (int*) malloc(sizeof(int));
     if (ptr != NULL) {
@@ -11,12 +11,14 @@ void* alloc_int(void) {
     return ptr;
 }
 
+//освобождение памяти
 void dealloc_int (void* ptr) {
     if (ptr != NULL) {
         free(ptr);
     }
 }
 
+//сложение a + b = result
 void add_int(const void* a, const void* b, void* result) {
     const int* arg1 = (const int*) a;
     const int* arg2 = (const int*) b;
@@ -25,6 +27,7 @@ void add_int(const void* a, const void* b, void* result) {
     *res = *arg1 + *arg2;
 }
 
+//умножение a * b = result
 void multiply_int(const void* a, const void* b, void* result) {
     const int* arg1 = (const int*) a;
     const int* arg2 = (const int*) b;
@@ -33,6 +36,7 @@ void multiply_int(const void* a, const void* b, void* result) {
     *res = *arg1 * *arg2;
 }
 
+//присвание result = arg
 void assign_int(void* result, const void* arg) {
     const int* src = (const int*) arg;
     int* dst = (int*)result;
@@ -40,11 +44,13 @@ void assign_int(void* result, const void* arg) {
     *dst = *src;
 }
 
+//вывод в консоль
 void print_int(const void* element) {
     const int* val = (const int*) element;
     printf("%d", *val);
 }
 
+//сравнение
 int compare_int(const void* a, const void* b) {
     const int* arg1 = (const int*) a;
     const int* arg2 = (const int*) b;
@@ -54,17 +60,19 @@ int compare_int(const void* a, const void* b) {
     return 0;
 }
 
+//запись нуля
 void zero_int(void* result) {
     int* res = (int*)result;
     *res = 0;
 }
 
+//запись единицы
 void one_int(void* result) {
     int* res = (int*)result;
     *res = 1;
 }
 
-
+//ленивая инициализация
 static TypeInfo* int_type_info = NULL;
 
 const TypeInfo* get_int_type_info(void) {
@@ -74,6 +82,8 @@ const TypeInfo* get_int_type_info(void) {
         if (int_type_info == NULL) {
             return NULL;
         }
+
+        //заполнение таблицы функций
         int_type_info->element_size = sizeof(int);
         int_type_info->add = add_int;
         int_type_info->multiply = multiply_int;
