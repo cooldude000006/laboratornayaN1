348b23ecd0d5ba662e4902b04c671fcad25a49cc
cooldude000006 <kivan000009@gmail.com>
2026-03-24 19:46:23 +0300

    Update int_type.h

--- int_type.h
+++ int_type.h
@@ -2,22 +2,3 @@
 #include "type_info.h"
 
 const TypeInfo* get_int_type_info(void);
-
-//вспомогательные функции для работы с int (функции внутри type_info)
-void* alloc_int(void);
-
-void dealloc_int(void* ptr);
-
-void add_int(const void* a, const void* b, void* result);
-
-void multiply_int(const void* a, const void* b, void* result);
-
-void assign_int(void* result, const void* arg);
-
-void print_int(const void* element);
-
-int compare_int(const void* a, const void* b);
-
-void zero_int(void* result);
-
-void one_int(void* result);
